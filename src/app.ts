2870d965443f69b4336f7a96b733a44cfcd17d3f
Artem Sheremet <dot.doom@gmail.com>
2019-04-19 16:07:49 +0200

    Improve error handling

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,10 @@ import * as bodyParser from 'body-parser';
 import escapeStringRegexp from 'escape-string-regexp';
 import { Application, RequestHandler } from 'express';
 import { dirSync } from 'tmp';
-import { URL } from 'url';
-import { Doku } from './doku';
+import { Doku, PageInfo } from './doku';
 import { Renderer } from './render';
 
-const preferredLanguage = (preferredLanguageCode: string): RequestHandler => {
+const clientLanguage = (preferredLanguageCode: string): RequestHandler => {
     return (req, res, next) => {
         const acceptLanguageHeader = req.header('Accept-Language');
         // If unspecified, language is the one server prefers.
@@ -33,6 +32,28 @@ const preferredLanguage = (preferredLanguageCode: string): RequestHandler => {
     }
 }
 
+const jsonApi = (handler: RequestHandler): RequestHandler => {
+    return async (req, res, next) => {
+        try {
+            let reply = handler(req, res, next);
+            while (reply instanceof Promise) {
+                reply = await reply;
+            }
+            if (reply !== undefined) {
+                res.json(reply);
+            }
+        } catch (e) {
+            console.error(
+                'Error while processing', req.url,
+                '\n-> PARAMS:', req.params,
+                '\n -> LOCALS:', res.locals,
+                '\n -> QUERY :', req.query,
+                '\n -> ', e.toString(), e.stack);
+            res.status(503).json(e.toString());
+        }
+    };
+}
+
 interface Comic {
     // Data available from $language:menu.
     id: string;
@@ -47,14 +68,16 @@ interface Comic {
     thumbnailURL?: URL;
 }
 
+interface Strip extends PageInfo {
+    title?: string;
+}
+
 export class App {
-    private readonly app: Application;
     private readonly render: Renderer;
     private readonly doku: Doku;
     private readonly baseUrl: URL;
 
     constructor(app: Application, render: Renderer, doku: Doku, baseUrl: URL) {
-        this.app = app;
         this.render = render;
         this.doku = doku;
         this.baseUrl = baseUrl;
@@ -62,14 +85,16 @@ export class App {
         app.use(
             bodyParser.urlencoded({ extended: true }),
             bodyParser.json(),
-            preferredLanguage('ru'),
+            clientLanguage('ru'),
         );
 
-        app.get('/strips/:id', async (req, res) => {
-            res.json({});
-        });
-        app.get('/strips/:id/render', this.stripRender);
-        app.get('/comics', this.comics);
+        app.get('/strips/:id', jsonApi(this.getStrip));
+        app.get('/strips/:id/render', jsonApi(this.renderStrip));
+
+        let comicsCache: Comic[] | undefined;
+        app.get('/comics', jsonApi(async (req, res) =>
+            (comicsCache = comicsCache ||
+                await this.getComics(res.locals.language))));
         app.get('/comics/:id', async (req, res) => {
 
         });
@@ -78,34 +103,35 @@ export class App {
         });
     }
 
-    private stripRender: RequestHandler = async (req, res) => {
+    private getStrip: RequestHandler = async (req, res) => {
         const pageId = [res.locals.language, req.params.id].join(':');
-        try {
-            const dir = dirSync();
-            try {
-                const page = await this.render.renderSinglePage(
-                    pageId, dir.name);
-                if (page.pageURL) {
-                    if (page.boxes.length == 1) {
-                        res.sendFile(page.boxes[0].path);
-                    } else {
-                        res.json(`${page.boxes.length} boxes found`)
-                            .status(400);
-                    }
-                } else {
-                    res.json('Page URL can not be computed').status(400);
-                }
-            } finally {
-                dir.removeCallback();
-            }
-        } catch (e) {
-            console.error(e);
-            res.json(`Error: ${e}`).status(503);
+        const pageText = await this.doku.getPage(pageId);
+
+        const strip: Strip = await this.doku.getPageInfo(pageId);
+        const titleMatch = pageText.match(/[*][*]([^*]+)[*][*]/);
+        if (titleMatch) {
+            strip.title = titleMatch[1];
         }
+
+        return strip;
     }
 
-    private comics: RequestHandler = async (req, res) => {
-        res.json(await this.getComics(res.locals.language));
+    private renderStrip: RequestHandler = async (req, res) => {
+        const pageId = [res.locals.language, req.params.id].join(':');
+
+        const dir = dirSync();
+        try {
+            const page = await this.render.renderSinglePage(pageId, dir.name);
+            if (!page.pageURL) {
+                throw new Error('Page URL can not be computed')
+            }
+            if (page.boxes.length != 1) {
+                throw new Error(`${page.boxes.length} boxes found`);
+            }
+            res.sendFile(page.boxes[0].path);
+        } finally {
+            dir.removeCallback();
+        }
     }
 
     private pageURL = (id: string) => new URL('/' + id.replace(/:/g, '/'),
@@ -121,29 +147,25 @@ export class App {
         //   $language:$comicId[:index]
         // we need $language and :index to form a URL, and otherwise throw them
         // away.
-        for (
-            let comicIdParser = (new RegExp(
-                '^(' +
-                escapeStringRegexp(language) +
-                ':)?(.*?)(:index)?$')).exec(comic.id); comicIdParser;) {
-            comic.id = comicIdParser[2];
+        let comicIdMatch = (new RegExp('^(' +
+            escapeStringRegexp(language) +
+            ':)?(.*?)(:index)?$')).exec(comic.id);
+        if (comicIdMatch) {
+            comic.id = comicIdMatch[2];
             comic.numberOfStrips = (await this.doku.getPagelist(
-                comicIdParser[1] + comicIdParser[2])).length;
-            break;
+                comicIdMatch[1] + comicIdMatch[2])).length;
         }
 
-        for (let titleMatch = indexPage.match(/=([^=]+?)=/); titleMatch;) {
+        let titleMatch = indexPage.match(/=([^=]+?)=/);
+        if (titleMatch) {
             comic.name = titleMatch[1].trim();
-            break;
         }
 
-        for (
-            let imageMatch = indexPage.match(
-                /{{([^}]+[.](png|jpe?g)[^|}]+)[^}]*}}/);
-            imageMatch;) {
+        let imageMatch = indexPage.match(
+            /{{([^}]+[.](png|jpe?g)[^|}]+)[^}]*}}/);
+        if (imageMatch) {
             comic.thumbnailURL = this.pageURL(
                 ['_media', comic.id, imageMatch[1].trim()].join('/'));
-            break;
         }
 
         return comic;
@@ -157,14 +179,8 @@ export class App {
         };
     }
 
-    private comicsCache?: Comic[];
-
     private getComics = async (language: string): Promise<Comic[]> => {
-        if (this.comicsCache) {
-            return this.comicsCache;
-        }
-
-        const menu = (await this.doku.getPage(`${language}:menu`)).split('\n');
+        const menu = (await this.doku.getPage(`${language}: menu`)).split('\n');
         const comics: Promise<Comic>[] = [];
         let categoryName: string | undefined = undefined;
         for (const line of menu) {
@@ -181,6 +197,6 @@ export class App {
                 }));
             }
         }
-        return (this.comicsCache = await Promise.all(comics));
+        return Promise.all(comics);
     }
 }
