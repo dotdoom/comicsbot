efb818ab2f1c795efb2442b507f6c9b6d4def62e
Artem Sheremet <dot.doom@gmail.com>
2020-05-08 19:34:49 +0200

    Bump viewport size artificially to fit strips

--- src/render.ts
+++ src/render.ts
@@ -34,7 +34,7 @@ export class Renderer {
     const browserPage = await this.browser.newPage();
     try {
       if (this.deviceScaleFactor) {
-        const viewport = await browserPage.viewport();
+        const viewport = browserPage.viewport();
         viewport.deviceScaleFactor = this.deviceScaleFactor;
         await browserPage.setViewport(viewport);
       }
@@ -42,10 +42,25 @@ export class Renderer {
       // TODO(dotdoom): when we have 18+ control in Discord.
       //await browserPage.setCookie(...this.doku.getCookies());
       await browserPage.goto(url.href);
+
+      const clip = (await browserPage.evaluate(render.findRect)) as DOMRect;
+      const viewport = browserPage.viewport();
+      if (viewport.height < clip.bottom || viewport.width < clip.right) {
+        console.log(
+          `resizing viewport ${viewport.width}x${viewport.height} to fit ` +
+            `rect [${clip.left}, ${clip.top}; ${clip.right}, ${clip.bottom}]`
+        );
+        // Must resize viewport to get the full element on the screen:
+        // https://github.com/puppeteer/puppeteer/issues/5080
+        viewport.width = Math.ceil(clip.right);
+        viewport.height = Math.ceil(clip.bottom);
+      }
+      await browserPage.setViewport(viewport);
+
       const renderFilename = this.renderFilename(url, baseDirectory);
-      console.info(`rendering page ${url} into ${renderFilename}`);
+      console.info(`rendering page ${url} rect into ${renderFilename}`);
       const pngBuffer = await browserPage.screenshot({
-        clip: (await browserPage.evaluate(render.findRect)) as DOMRect,
+        clip: clip,
       });
       mkdirp.sync(path.dirname(renderFilename));
       await sharp(pngBuffer).webp().toFile(renderFilename);
